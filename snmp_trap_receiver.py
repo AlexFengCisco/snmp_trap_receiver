5be75b35a780edde610b7c1fc08c592c6f886069
AlexFenfgCiso <alfeng@cisco.com>
2019-01-29 16:32:56 +0800

    README.md result_output decode_description snmp_trap_receiver sample code

--- snmp_trap_receiver.py
+++ snmp_trap_receiver.py
@@ -21,8 +21,6 @@ def print_snmp_trap(position,key,value):
     global snmp_ver
     global positon_bias
 
-    #print(position)
-    #print(positon_bias)
 
     if position == 3 or position == 2: # in case of len occupied two bytes
         print(key+' '+str(value)+ ' [snmp version '+snmp_ver+']')
@@ -86,13 +84,11 @@ def parse_snmp_trap(snmp_trap_content):
     i = 0
     while True:
         hit = 0
-        # print(snmp_trap_content[i])
 
         if i >= snmp_trap_content_len:
             break
         while snmp_trap_content[i] == 48: #0x30
             hit = 1
-            # print('Sequence list  '+str(i))
             list_index = i
             list_len = snmp_trap_content[i + 1]
             if list_len == 129: #0x81
@@ -105,7 +101,7 @@ def parse_snmp_trap(snmp_trap_content):
                     if buf[5] == 1:
                         snmp_ver = 'v2c'
                 i = i+2+1    
-                #print('Snmp Trap Content List len ' + str(list_len))
+
             else:
                 if i == 0:
 
@@ -138,7 +134,6 @@ def parse_snmp_trap(snmp_trap_content):
             comm_str_len = snmp_trap_content[i+1]
             comm_str = snmp_trap_content[i+2:i+2+comm_str_len]
             print_snmp_trap(i,key,comm_str)
-            #print('OCT STRING '+str(comm_str))
             i = i+2+comm_str_len
             break
 
@@ -150,7 +145,6 @@ def parse_snmp_trap(snmp_trap_content):
             comm_str_len = snmp_trap_content[i+1]
             comm_str = snmp_trap_content[i+2:i+2+comm_str_len]
             print_snmp_trap(i,key,comm_str)
-            #print('OCT STRING IP ADDR '+str(comm_str))
             i = i+2+comm_str_len
             break
 
@@ -160,11 +154,8 @@ def parse_snmp_trap(snmp_trap_content):
             hit = 1
             key = 'OID'
             object_index = i
-            # print('object identifier '+str(snmp_trap_content[i]))
             object_len = snmp_trap_content[i + 1]
-            # print('object len '+str(object_len))
             OID_num = snmp_trap_content[object_index + 2:object_index + 2 + object_len]
-            # print('OID '+str(snmp_trap_content[object_index+2:object_index+2+object_len]))
             OID_str = []
             for OID_i in OID_num:
                 OID_str.append('.' + str(OID_i))
@@ -173,7 +164,6 @@ def parse_snmp_trap(snmp_trap_content):
                 OID_str[0] = '1.3'
             OID = ''.join(map(str, (OID_str)))
             print_snmp_trap(i,key,OID)
-            #print('OID '+OID)
             i = i + 2 + object_len
             break
 
@@ -198,13 +188,12 @@ def parse_snmp_trap(snmp_trap_content):
             int_len = snmp_trap_content[i + 1]
             int_num = int(snmp_trap_content[int_index + 2:int_index + 2 + int_len].hex(), 16)
             print_snmp_trap(i,key,int_num)
-            #print('INTEGER ' + str(int_num))
             i = i + 2 + int_len
             break
 
         if hit == 0:
             i += 1
-        # print('index '+str(i))
+
         if i >= snmp_trap_content_len:
             break
 
@@ -224,29 +213,10 @@ while True:
     snmp_ver = ''
 
     buf, addr = sock.recvfrom(10000)
-    #print(buf)
     len_buf = (len(buf))
-    #print("{0:x}".format(len_buf))
     hex_buf = (buf.hex())
-    #print(hex_buf)
-    #print(type(hex_buf)) #str
-    #print(type(buf))
-    #print(type(str(addr)))   #str
     print("Snmp Trap message from {0:s}".format(str(addr)))
 
-    
-    '''
-       print(bytes.fromhex('7075626c6963'))
-       >b'public'
-       
-       
-       hex_str='0x73'
-       int_dec = int(hex_str,16)
-       print(int_dec)
-       >115
-
-       
-    '''
     parse_snmp_trap(buf)
 
     print('-'*100)
